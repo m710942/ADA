67aa5d89c67cbecfbe1243c74633aec7853dcc8d
m710942 <ms710942@iteso.mx>
2016-11-16 15:40:12 -0600

    Testing cookie redirect

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 
 $(document).ready(function () {
     if(window.location.hash){
-        document.write("Hello "+window.location.hash);      
+       document.cookie = "access_token="+window.location.hash;
+       document.location="index.html";     
     }else{
-       
+       checkCookie();
     }
   
 });
+
+
+getCookie = function(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+checkCookie = function() {
+    var token=getCookie("access_token");
+    if (token!= "") {
+        alert("Welcome again " + token);
+    } 
+}
