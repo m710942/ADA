a79e73bd85633332e4cc2d76e23984e0aefba185
Guillermo Mauricio Robles Martinez <gmromar>
2016-11-30 15:59:06 -0600

    added defaults

--- js/media.js
+++ js/media.js
@@ -3,15 +3,14 @@ var MEDIA = MEDIA || {};
 
 MEDIA.decorate_ig = function(media, json){
 	media.captions = {};
-	media.captions.user = json.caption;
-	media.tags = json.tags;
-	media.comments = json.comments;
-	media.likes = json.likes;
+	media.captions.user = json.caption || {text: "---"};
+	media.tags = json.tags || [];
+	media.comments = json.comments || {count: 0};
+	media.likes = json.likes || {count: 0};
 	media.standard_resolution = json.images.standard_resolution;
 	media.thumbnail = json.images.thumbnail;
 	media.id = json.id;
 	media.type	= json.type;
-	media.tags = json.tags;
 
 	return media;
 };
