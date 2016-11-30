b0fd0c58a77eeca7d27f9e721f89f66cdb3ab375
m710942 <ms710942@iteso.mx>
2016-11-29 22:50:42 -0600

    Update media.js

--- js/media.js
+++ js/media.js
@@ -5,6 +5,8 @@ MEDIA.decorate_ig = function(media, json){
 	media.captions = {};
 	media.captions.user = json.caption;
 	media.tags = json.tags;
+	media.comments = json.comments;
+	media.likes = json.likes;
 	media.standard_resolution = json.images.standard_resolution;
 	media.thumbnail = json.images.thumbnail;
 	media.id = json.id;
@@ -19,7 +21,7 @@ MEDIA.decorate_ms = function(media, json){
 	media.color = json.color;
 	media.people = json.faces;
 	media.clazz = json.tags;
-	media.tags.push(json.description.tags);
+	media.tags.concat(json.description.tags);
 
 	return media;
 };
