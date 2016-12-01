d28e82787af17cc23fef02b89be02a572c1b9322
Guillermo Mauricio Robles Martinez <gmromar>
2016-12-01 09:02:18 -0600

    missing symbol

--- js/search.js
+++ js/search.js
@@ -39,7 +39,7 @@ buscar = function(token){
   TOKEN = token;
   $("#working").show();
   $("#alert").hide();
-  $.ajax("https://api.instagram.com/v1/tags/"+CURRENT_SEARCH +"/media/recent?access_token="+TOKEN, {dataType:'jsonp' data:{format: 'json'}, jsonp:'callback', success:function(data){
+  $.ajax("https://api.instagram.com/v1/tags/"+CURRENT_SEARCH +"/media/recent?access_token="+TOKEN, {dataType:'jsonp', data:{format: 'json'}, jsonp:'callback', success:function(data){
     onMessageReceived(data);
   }});
 }
